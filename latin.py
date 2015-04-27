6ef6a938d751219e27c61dcb4397b316fc56a1ee
aruchames <ruchames@princeton.edu>
2015-04-27 02:24:24 -0400

    Not much really

---
+++ latin.py
@@ -0,0 +1,9 @@
+""" Andrew Ruchames
+	A translation parser for Latin. This will have functionality similar to that of William Whittaker's words.
+	
+"""
+input = raw_input()
+input.split(" ")
+for word in input
+	if (	
+			
