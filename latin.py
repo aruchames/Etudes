5a1b55319224c10732b7d8bb38ebec326ff26edf
aruchames <ruchames@princeton.edu>
2015-09-08 20:22:31 +0000

    Changed project direction

--- latin.py
+++
@@ -1,9 +0,0 @@
-""" Andrew Ruchames
-	A translation parser for Latin. This will have functionality similar to that of William Whittaker's words.
-	
-"""
-input = raw_input()
-input.split(" ")
-for word in input
-	if (	
-			
